54569b91bdfe6c256fea7ed21046d0f7aac2f61b
devanshuchaudhary <devanshu88@gmail.com>
2023-02-14 01:21:29 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by dddiehard on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
